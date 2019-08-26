58c062b6915b8baa90a2bbf433225ee130954312
Rakesh Jangir <rakeshj@letsenkindle.com>
2019-08-26 14:28:26 +0530

    Branch Rakesh Created

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '172.16.3.124',
+	'hostname' => '172.16.3.123',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'mineral_water',
