f62fcfeae813be6df47aff02ec4a32ea8d21cdce
Rakesh Jangir <rakeshj@letsenkindle.com>
2019-08-27 13:08:55 +0530

    flash bug resolved

--- application/views/layouts/flash.php
+++ application/views/layouts/flash.php
@@ -7,6 +7,15 @@
 </div>
 <?php endif; ?>
 
+<?php if($this->session->flashdata('success')) : ?>
+    <div class="alert alert-block alert-success">
+        <button type="button" class="close" data-dismiss="alert">
+            <i class="ace-icon fa fa-times"></i>
+        </button>
+        <?php echo $this->session->flashdata('success'); ?>
+    </div>
+<?php endif; ?>
+
 <?php if($this->session->flashdata('error')) : ?>
 <div class="alert alert-block alert-danger">
 	<button type="button" class="close" data-dismiss="alert">
