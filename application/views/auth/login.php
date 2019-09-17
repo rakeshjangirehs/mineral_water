2e918a3c733a300065bc5628e1a9f9da5d331995
Milan Soni <milans@ehealth.local>
2019-09-17 14:26:59 +0530

    w3c compliant

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -153,21 +153,21 @@
 <![endif]-->
     <!-- Warning Section Ends -->
     <!-- Required Jquery -->
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/jquery/js/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/jquery-ui/js/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/popper.js/js/popper.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/jquery/js/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/jquery-ui/js/jquery-ui.min.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/popper.js/js/popper.min.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/bootstrap/js/bootstrap.min.js"></script>
     <!-- jquery slimscroll js -->
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/jquery-slimscroll/js/jquery.slimscroll.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/jquery-slimscroll/js/jquery.slimscroll.js"></script>
     <!-- modernizr js -->
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/modernizr/js/modernizr.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/modernizr/js/css-scrollbars.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/modernizr/js/modernizr.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/modernizr/js/css-scrollbars.js"></script>
     <!-- i18next.min.js -->
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/i18next/js/i18next.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/i18next-xhr-backend/js/i18nextXHRBackend.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/bower_components/jquery-i18next/js/jquery-i18next.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>files/assets/js/common-pages.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/i18next/js/i18next.min.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/i18next-xhr-backend/js/i18nextXHRBackend.min.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js"></script>
+    <script src="<?php echo base_url(); ?>files/bower_components/jquery-i18next/js/jquery-i18next.min.js"></script>
+    <script src="<?php echo base_url(); ?>files/assets/js/common-pages.js"></script>
 
 <script>
 
